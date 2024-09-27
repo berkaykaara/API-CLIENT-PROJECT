2831aa87b876ba8040ada653090cd78ce52b00c5
berkaykaara <berkay.kara02@bilgiedu.net>
2024-09-27 18:59:57 +0300

    AAAA

--- AspNetCoreWebAPIClientProject/obj/Release/net8.0/AspNetCoreWebAPIClientProject.AssemblyInfo.cs
+++ AspNetCoreWebAPIClientProject/obj/Release/net8.0/AspNetCoreWebAPIClientProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreWebAPIClientProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66928d49603a4ae3ee52b0cf8be0b60a92d3049")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreWebAPIClientProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreWebAPIClientProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
